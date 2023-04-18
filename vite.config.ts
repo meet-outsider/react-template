ac3605c0246e24871aeab9fc40dfd6a3fda5b6ab
outsider <meet.outsider@gmail.com>
2023-04-18 22:29:47 +0800

    :recycle: refactor(deps): project-layout
    modify layout and add features

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,33 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-});
+import { join } from 'path';
+import { defineConfig, loadEnv } from 'vite';
+import { createHtmlPlugin } from 'vite-plugin-html';
+export default (mode: string) => {
+  const env = loadEnv(mode, process.cwd());
+  return defineConfig({
+    base: './',
+    plugins: [
+      createHtmlPlugin({
+        inject: {
+          data: {
+            title: env.VITE_APP_TITLE,
+          },
+        },
+      }),
+    ],
+    server: {
+      port: Number(env.VITE_PORT),
+      proxy: {
+        '/api': {
+          target: env.VITE_BACKEND_URL,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+        },
+      },
+    },
+    resolve: {
+      alias: {
+        '@': join(__dirname, 'src'),
+      },
+    },
+  });
+};
